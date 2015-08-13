1b83e04ad9b069557050778c6d08dbe4442748df
Max Kaye <m@xk.io>
2015-08-14 08:35:55 +1000

    Begin transition to XT nodes

--- bitcoinAutoNode.sh
+++ bitcoinAutoNode.sh
@@ -12,7 +12,10 @@ echo "########### Updating Ubuntu"
 apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade -y
-apt-get install software-properties-common python-software-properties -y
+apt-get install software-properties-common python-software-properties htop -y
+apt-get -y install git build-essential autoconf libboost-all-dev libssl-dev pkg-config
+apt-get -y install libprotobuf-dev protobuf-compiler libqt4-dev libqrencode-dev libtool
+apt-get -y install libcurl4-openssl-dev
 echo "########### Creating Swap"
 dd if=/dev/zero of=/swapfile bs=1M count=1024 ; mkswap /swapfile ; swapon /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
@@ -20,8 +23,17 @@ echo "########### Adding ppa:bitcoin/bitcoin and installing bitcoind"
 add-apt-repository -y ppa:bitcoin/bitcoin
 apt-get update -y
 mkdir ~/.bitcoin/
-apt-get install bitcoind -y
+apt-get install db4.8
+echo "########### Cloning XT and Compiling"
+mkdir -p ~/src && cd ~/src
+git clone https://github.com/bitcoinxt/bitcoinxt.git
+cd bitcoinxt
+./autogen.sh
+./configure --without-gui --without-upnp
+make
+make install
 echo "########### Creating config"
+cd ~/
 config=".bitcoin/bitcoin.conf"
 touch $config
 echo "server=1" > $config
@@ -31,9 +43,11 @@ randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 echo "rpcuser=$randUser" >> $config
 echo "rpcpassword=$randPass" >> $config
+echo "prune=10000" >> $config # safe enough for now
 echo "########### Setting up autostart (cron)"
 crontab -l > tempcron
 echo "@reboot bitcoind -daemon" >> tempcron
+echo "0 3 * * * reboot" >> tempcron
 crontab tempcron
 rm tempcron
 reboot
